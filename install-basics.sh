f948b284b8b734c44ada7bd74bbda615a56f2cff
Matthias Kümmel <mkuemmel26@gmail.com>
2022-04-30 17:24:32 +0200

    Create install-basics.sh

---
+++ install-basics.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y vim
+cat <<EOT >> ~/.vimrc
+let data_dir = has('nvim') ? stdpath('data') . '/site' : '~/.vim'
+if empty(glob(data_dir . '/autoload/plug.vim'))
+    silent execute '!curl -fLo '.data_dir.'/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+    autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
+endif
+
+
+call plug#begin(expand('~/.vim/plugged'))
+Plug 'arcticicestudio/nord-vim'
+call plug#end()
+
+colorscheme nord
+EOT
